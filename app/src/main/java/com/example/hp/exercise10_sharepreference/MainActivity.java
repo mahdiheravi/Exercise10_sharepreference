2f6c2e4ba66488639d2ef64c9b2fdde77b323988
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-31 12:11:45 +0330

    Initialize

---
+++ app/src/main/java/com/example/hp/exercise10_sharepreference/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hp.exercise10_sharepreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

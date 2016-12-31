a806cdc416260882f9128f7963f2c19607936fb9
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-31 15:46:16 +0330

    Set Setting keyif Not Login

--- app/src/main/java/com/example/hp/exercise10_sharepreference/MainActivity.java
+++ app/src/main/java/com/example/hp/exercise10_sharepreference/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hp.exercise10_sharepreference;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,7 +16,31 @@ public class MainActivity extends AppCompatActivity {
         Button btnenter = (Button) findViewById(R.id.btnenter);
         EditText txtusername = (EditText) findViewById(R.id.txtusername);
         EditText txtpass = (EditText) findViewById(R.id.txtpass);
-        
+        final Pref mypref = new Pref(MainActivity.this);
+        final Setting mysetting = new Setting();
+        mysetting.setUsername(txtusername.getText().toString());
+        mysetting.setPassword(txtpass.getText().toString());
+        btnenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mypref.setsetting(mysetting);
+
+            }
+        });
+
+
+
+
+
+      //  mypref.initialize();
+
+       mysetting.setUsername(mypref.getsetting().getUsername());
+     // if (mysetting.getUsername()=="")
+
+
+
+
+
 
     }
 }
--- app/src/main/java/com/example/hp/exercise10_sharepreference/Pref.java
+++ app/src/main/java/com/example/hp/exercise10_sharepreference/Pref.java
@@ -2,6 +2,7 @@ package com.example.hp.exercise10_sharepreference;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.google.gson.Gson;
 /**
@@ -32,6 +33,22 @@ public class Pref {
         Gson gson  = new Gson();
         editor.putString(SettingKey,gson.toJson(input));
         editor.commit();
+    }
+    public void initialize()
+    {
+
+        Setting empty =  new Setting();
+
+        empty.setUsername("");
+        empty.setUsername("");
+        empty.setThemecolor("");
+        Gson gson  = new Gson();
+
+        editor.putString(SettingKey,gson.toJson(empty));
+
+        editor.commit();
+
+
     }
 
 

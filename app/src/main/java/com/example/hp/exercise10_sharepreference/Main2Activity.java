51d14ee820b6bfcb0a7df679f2324f7145095b8c
hp-PC\hp <Mahdi.heravi@gmail.com>
2017-01-04 13:25:40 +0330

    SET OnClickListener for Color Buttons

--- app/src/main/java/com/example/hp/exercise10_sharepreference/Main2Activity.java
+++ app/src/main/java/com/example/hp/exercise10_sharepreference/Main2Activity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -21,9 +22,33 @@ public class Main2Activity extends AppCompatActivity {
 
         mysetting = (Setting) Myintent.getParcelableExtra("setting");
 
-        Button btnred = (Button) findViewById(R.id.btnenter);
+        Button btnred = (Button) findViewById(R.id.btnred);
+        btnred.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ThemeColor[0]="RED";
+               // Toast.makeText(Main2Activity.this, "RED", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         Button btngreen = (Button) findViewById(R.id.btngreen);
+        btngreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ThemeColor[0]="GREEN";
+                //Toast.makeText(Main2Activity.this, "GREEN", Toast.LENGTH_SHORT).show();
+
+            }
+        });
         Button btnblue = (Button) findViewById(R.id.btnblue);
+        btnblue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ThemeColor[0]="BLUE";
+               // Toast.makeText(Main2Activity.this, "BLUE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         TextView mytext = (TextView) findViewById(R.id.textView);
         Button btnlogout = (Button) findViewById(R.id.btnlogout);
         btnlogout.setOnClickListener(new View.OnClickListener() {
@@ -36,11 +61,11 @@ public class Main2Activity extends AppCompatActivity {
                 mysetting.setPassword("");
                 mypref.setsetting(mysetting);
 
-                Log.d("myintent1", "logout");
+               // Log.d("myintent1", "logout");
                 finish();
                // Intent Myintent2 = new Intent(Main2Activity.this, MainActivity.class);
                // startActivity(Myintent2);
-                Log.d("========", "myintent2");
+                //Log.d("========", "myintent2");
 
             }
         });
